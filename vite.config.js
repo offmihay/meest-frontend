acf8544fe170945371633eae1299c79d9a781af5
Volodymyr <offmihay@gmail.com>
2024-05-11 20:20:47 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   server: {
     port: 80,
   },
-  base: "/Meest-calculator-test"
-})
+});
